ff7e718c1eb3c15414133cb88ed3c1f14f8cfacf
Ellinor Johansson <ellinor.asaka@gmail.com>
2025-01-25 12:16:45 +0100

    📝 add comment

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,11 @@ function getRandomQuote(): string {
   return quotes[randomIndex];
 }
 
+// Hämtar knappen och p taggen där citaten ska ligga.
 const quoteButton = document.getElementById('quoteButton') as HTMLButtonElement;
 const quoteDisplay = document.getElementById('quoteDisplay') as HTMLParagraphElement;
 
+// När man klickar på knappen så visas ett quote som valdes i getRandomQuote.
 quoteButton.addEventListener('click', () => {
   const randomQuote = getRandomQuote();
   quoteDisplay.textContent = randomQuote;

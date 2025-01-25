4da93ed90f3a41cac9bf641b95fae6041ab15eaa
Ellinor Johansson <ellinor.asaka@gmail.com>
2025-01-25 12:38:11 +0100

    ♻️changed quotes

--- src/ts/quotes.ts
+++ src/ts/quotes.ts
@@ -1,9 +1,14 @@
 export const quotes: string[] = [
-	"The best way to predict the future is to create it.",
-	"Success is not final; failure is not fatal: It is the courage to continue that counts.",
-	"Don't watch the clock; do what it does. Keep going.",
-	"You only live once, but if you do it right, once is enough.",
-	"Believe you can and you're halfway there."
+	"I'm not lazy, I'm just on energy-saving mode.",
+  "I haven't failed, I've just found 10,000 ways that won't work.",
+  "If you want something done, give it to a busy person. Or a cat—they have nine lives to manage it.",
+  "I'm not great at math, but I'm pretty sure pizza + ice cream = happiness.",
+  "Being an adult is like folding a fitted sheet—nobody really knows how.",
+  "I do exercise. Sometimes I lift the remote control multiple times in a row.",
+  "There are three types of people in the world: those who can count and those who can't.",
+  "Why don't skeletons fight? They don't have the guts.",
+  "Life's too short to take seriously. So, laugh while you still have teeth!",
+  "I told my computer I needed a break, and now it won't stop sending me KitKats."
 ];
 
 export default quotes;

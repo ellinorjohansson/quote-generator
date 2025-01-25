6ec5f3431ca60a5a417e983a9be2571d8b4ee4ac
Ellinor Johansson <ellinor.asaka@gmail.com>
2025-01-25 11:52:08 +0100

    ✨ generate quote

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,16 @@
 import "./style.scss";
+import quotes from "./ts/quotes";
 
+// Genererar ett slumpmässigt tal och multiplicerar med antalet som finns i arrayen och avrundar ner till heltal för att matcha arrayen. Returnerar sen ett quote baserat på indexet som slumpmässigt togs fram.
+function getRandomQuote(): string {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  return quotes[randomIndex];
+}
+
+const quoteButton = document.getElementById('quoteButton') as HTMLButtonElement;
+const quoteDisplay = document.getElementById('quoteDisplay') as HTMLParagraphElement;
+
+quoteButton.addEventListener('click', () => {
+  const randomQuote = getRandomQuote();
+  quoteDisplay.textContent = randomQuote;
+})
---
+++ src/ts/quotes.ts
@@ -0,0 +1,9 @@
+export const quotes: string[] = [
+	"The best way to predict the future is to create it.",
+	"Success is not final; failure is not fatal: It is the courage to continue that counts.",
+	"Don't watch the clock; do what it does. Keep going.",
+	"You only live once, but if you do it right, once is enough.",
+	"Believe you can and you're halfway there."
+];
+
+export default quotes;
